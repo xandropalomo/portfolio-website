b15338b5ca2e3aa9f4a03c9592ae8b4cdf01bde0
xandropalomo <145553546+xandropalomo@users.noreply.github.com>
2024-12-08 16:27:33 +0100

    deel opdracht 4

--- js/map.js
+++ js/map.js
@@ -1,5 +1,5 @@
 // Initialiseer de kaart op het opgegeven adres
-const map = L.map('map').setView([51.181927, 4.364613], 14); // Zoomniveau iets verder uitgezoomd
+const map = L.map('map').setView([51.181927, 4.364613], 12); // Zoomniveau iets verder uitgezoomd
 
 // Voeg de kaartlaag toe
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -11,5 +11,5 @@ L.circle([51.181927, 4.364613], {
   color: 'red', // Randkleur
   fillColor: '#f03', // Vulkleur
   fillOpacity: 0.4, // Doorzichtigheid
-  radius: 1000 // Grote straal: 1 kilometer
+  radius: 1500 // Grote straal: 1.5 kilometer
 }).addTo(map);
